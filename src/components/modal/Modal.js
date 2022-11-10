ad4b2eb6399ce0ab248f1368a3a50c2bf0b05159
Hasime Merovci <96388862+Hasime1@users.noreply.github.com>
2022-11-10 17:58:16 +0100

    a commit

---
+++ src/components/modal/Modal.js
@@ -0,0 +1,143 @@
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import "./modal.css";
+import { Plus } from "react-feather";
+
+function ModalDiv({ showModal, setShowModal, refresher }) {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [background, setBackground] = useState("#F9F5EB");
+  const [foreground, setForeground] = useState("#fff");
+  const [priority, setPriority] = useState("normal");
+
+  const handleAdd = () => {
+    const savedData = JSON.parse(localStorage.getItem("myNotes")) || [];
+    if (!title || !content) {
+      return alert("Title and Content is required");
+    }
+    let newData = {
+      id: Date.now(),
+      title,
+      content,
+      priority,
+      background,
+      foreground,
+      date: new Date().toLocaleDateString(),
+    };
+    savedData.push(newData);
+    localStorage.setItem("myNotes", JSON.stringify(savedData));
+    setTitle("");
+    setContent("");
+    setPriority("normal");
+    setShowModal(false);
+    // window.location.reload()
+    refresher();
+  };
+
+  const handleCancel = () => {
+    setTitle("");
+    setContent("");
+    setPriority("normal");
+    setShowModal(false);
+  };
+
+  const handleColor = (bg, fg) => {
+    setBackground(bg);
+    setForeground(fg);
+  };
+
+  return (
+    <>
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add New Note</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input
+            className="form-control mb-3"
+            placeholder="Enter title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label>Priority</label>
+          <select
+            className="form-control mb-3"
+            value={priority}
+            onChange={(e) => setPriority(e.target.value)}
+          >
+            <option value="normal">Normal</option>
+            <option value="high">High</option>
+          </select>
+          <textarea
+            className="form-control"
+            style={{ height: "180px" }}
+            placeholder="Enter notes...."
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          ></textarea>
+          {/* for theme selection */}
+          <DropdownButton id="dropdown-basic-button" title="Select Theme">
+            <Dropdown.Item href="#/action-1">
+              <div
+                className="d-flex"
+                onClick={() => handleColor("#bee0ec", "#205e77")}
+              >
+                <div
+                  className="circle"
+                  style={{ backgroundColor: "#bee0ec" }}
+                ></div>{" "}
+                <div
+                  className="circle "
+                  style={{ backgroundColor: "#205e77" }}
+                ></div>
+              </div>{" "}
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-2">
+              <div
+                className="d-flex"
+                onClick={() => handleColor("#FFE898", "#FFF8BC")}
+              >
+                <div
+                  className="circle"
+                  style={{ backgroundColor: "#FFE898" }}
+                ></div>{" "}
+                <div
+                  className="circle "
+                  style={{ backgroundColor: "#FFF8BC" }}
+                ></div>
+              </div>
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-3">
+              <div
+                className="d-flex"
+                onClick={() => handleColor("#AfB4FF", "#B1E1FF")}
+              >
+                <div
+                  className="circle"
+                  style={{ backgroundColor: "#AfB4FF" }}
+                ></div>{" "}
+                <div
+                  className="circle "
+                  style={{ backgroundColor: "#B1E1FF" }}
+                ></div>
+              </div>
+            </Dropdown.Item>
+          </DropdownButton>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCancel}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={handleAdd}>
+            <Plus /> Add
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default ModalDiv;
